14002adaf3cf51b7d8a1d75ea0aca5aa3aa33265
junkfoodpenguin <junkfoodpenguin@JPs-MacBook-Pro.local>
2021-09-01 01:52:58 -0700

    first stab at adding links

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import siteContent from '../contents/';
+
+function App() {
+  const now = new Date();
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        Works by e-sare
+      </header>
+      <main className="app-content">
+        { 
+          siteContent.links.map((c, idx) => (
+            <section 
+              className="content-section"
+              key={idx}>
+                <div className="content-section__img-content content-section__item">
+                  <img className="content-section__img" src={c.imgSrc}/>
+                </div>
+                <div className="content-section__description-content content-section__item">
+                  <h2>{c.title}</h2>
+                  <p>{c.about}</p>
+                  <div className="content-section__cta">
+                    <a
+                      href={c.href}
+                      target="_blank"
+                      rel="noreferrer">
+                      <button className="button">See more</button>
+                    </a>
+                  </div>
+                  { c.extra && (
+                    <div className="content-section__extra">
+                      <a
+                        href={c.extra?.href}
+                        target="_blank"
+                        rel="noreferrer">
+                        {c.extra?.cta}
+                      </a>
+                    </div>
+                  ) }
+                </div>
+            </section>
+          ))
+        }
+      </main>
+      <footer className="app-footer">
+        <div>{now.getFullYear()} by e-sare</div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/contents/index.js
@@ -0,0 +1,48 @@
+const siteContent = {
+  links: [
+    {
+      title: 'Google Assistant Developer Pages',
+      about: 'Google Assistant Developer pages refresh initiative.',
+      href: 'https://developers.google.com/assistant',
+      imgSrc: './sample-links/images/g_assistant.png',
+    },
+    {
+      title: 'Google\'s Coding Competitions',
+      about: 'Google Coding Competition front-end redesign.',
+      href: 'https://codingcompetitions.withgoogle.com/',
+      imgSrc: './sample-links/images/g_competition.png',
+    },
+    {
+      title: 'Verizon Express',
+      about: 'A next generation retail store experience that with multiple digital touch point that aims to streamline the retail experience.',
+      href: 'https://www.akqa.com/work/ibm/museum-of-the-unseen/',
+      imgSrc: 'https://images.ctfassets.net/feazk3r7m969/4C2dL3EwAoPgJQEHdFlVgr/c858f21e0d7e1d0c3e16eadb30c06491/vzn-hero-case-study-v2.png?w=3840&fm=webp',
+    },
+    {
+      title: 'IBM Museum of the Unseen',
+      about: 'An interactive bookshelf for IBM\'s smart museum in Germany.',
+      href: 'https://www.akqa.com/work/ibm/museum-of-the-unseen/',
+      imgSrc: 'https://images.ctfassets.net/feazk3r7m969/3Tj2KqOckyoQ2JXUFpgzwB/433ba47d6d03f092dcc5b9fa54bf56ef/IBM_Watson_Munich_Case_Study_v21.00_00_40_00.Still003_copy.jpg?w=3840&q=70&fm=webp',
+    },
+    {
+      title: 'IBM Triple-Checked',
+      about: 'An interactive installation that leverages IBM\'s visual recognition and machine learning technologies to suggest users how they can protect their data from most popular apps.',
+      href: 'https://www.akqa.com/work/ibm/museum-of-the-unseen/',
+      imgSrc: 'https://images.ctfassets.net/feazk3r7m969/3pxmqXEl9KKvLrO7eMGpfS/d033b67fed2a5609da80a0a556511b98/IBM_think_hero_copy.jpg?w=3840&q=70&fm=webp',
+    },
+    {
+      title: 'Destiny 2 Ghost Skill',
+      about: 'The official Amazon Alexa skill for Destiny 2 the game. Bringing the Ghost character directly into the player\'s home, powered by the Amazon Alexa.',
+      href: 'https://www.akqa.com/work/activision/destiny-2-ghost-skill/',
+      imgSrc: 'https://images.ctfassets.net/feazk3r7m969/1snHnUNE4sB1IvJodxpvTf/c1874bdb9e8d8b6c161be975387e3e73/article-01-a-large-2.jpg?w=3840&q=70&fm=webp',
+      extra: {
+        href: 'https://www.destinythegame.com/Ghost',
+        cta: 'Destiny 2 Ghost Skill offical site'
+      }
+    },
+  ],
+}
+
+export default siteContent;
+
+
